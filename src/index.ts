106bf9944835429f03deccb912b6392a502d865c
sarvajeet-hue <sarvajeet.1923en1095@kiet.edu>
2024-08-29 15:52:11 +0530

    First Commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+
+import { WebSocketServer } from "ws";
+import http from 'http'
+
+const server = http.createServer(function(request: any, response: any) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    
+    response.end("hi there");
+});
+
+    const wss = new WebSocketServer({server})
+
+    wss.on('connection' , function connection(ws) {
+        ws.on('error' , console.error)
+        ws.on('message' , function message(data , isBinary){
+            wss.clients.forEach(function each(client) {
+                
+            })
+        })
+    })
+
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
